f090823e78a3ec2a986986991592c480981dd341
Owen Jones <jones@biology.sdu.dk>
2021-07-07 11:17:06 +0200

    Added spelling check script and associated files

---
+++ SpellCheckScript.R
@@ -0,0 +1,10 @@
+#Spell check script
+library(spelling)
+
+rmd_files<-list.files(pattern = "*.Rmd",full.names = TRUE)
+
+wordlist <- readLines("spellCheck_wordlist")
+
+
+(x <- spelling::spell_check_files(path = rmd_files,ignore = wordlist, lang = "en_GB"))
+writeLines(x[["word"]],"spellcheckOut.csv")
