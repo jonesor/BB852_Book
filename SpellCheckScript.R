6cf3c9cddb38ce696aa07ae60c0a7323b82e31a8
Owen Jones <jonesor@gmail.com>
2023-05-10 12:46:00 +0200

    Prep for 2023 course

--- SpellCheckScript.R
+++ SpellCheckScript.R
@@ -3,7 +3,7 @@ library(spelling)
 
 rmd_files <- list.files(pattern = "*.Rmd", full.names = TRUE)
 
-wordlist <- readLines("spellCheck_wordlist")
+wordlist <- readLines("spellCheck_wordlist.txt")
 
 
 (x <- spelling::spell_check_files(path = rmd_files, ignore = wordlist, lang = "en_GB"))
