a56736b7aee2deaa80c88ea04b89dc40d1f113d0
Owen Jones <jonesor@gmail.com>
2023-05-10 16:15:51 +0200

    Update SpellCheckScript.R

--- SpellCheckScript.R
+++ SpellCheckScript.R
@@ -6,5 +6,9 @@ rmd_files <- list.files(pattern = "*.Rmd", full.names = TRUE)
 wordlist <- readLines("spellCheck_wordlist.txt")
 
 
-(x <- spelling::spell_check_files(path = rmd_files, ignore = wordlist, lang = "en_GB"))
+(x <- spelling::spell_check_files(
+  path = rmd_files,
+  ignore = wordlist,
+  lang = "en_GB"
+))
 writeLines(x[["word"]], "spellcheckOut.csv")
